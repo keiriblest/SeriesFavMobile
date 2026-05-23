671b5dec857875ed9ce2ffff85039329eab24ccf
keiriblest <keiriblest@gmail.com>
2026-05-24 00:42:09 +0200

    Fix formatting in build.gradle.kts dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.12.0
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime)
     implementation(libs.androidx.activity.compose)
